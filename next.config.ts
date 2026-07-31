84f6d9f2e5402443a00631d326c3a6346a218485
KlyrhonMiko <aurelklyrhonmiko@gmail.com>
2026-08-01 06:21:17 +0800

    fix: 404 error

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,11 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/koin',
-        destination: 'https://koin-web-two.vercel.app/',
+        destination: 'https://koin-web-two.vercel.app/koin',
       },
       {
         source: '/koin/:path*',
-        destination: 'https://koin-web-two.vercel.app/:path*',
+        destination: 'https://koin-web-two.vercel.app/koin/:path*',
       },
     ];
   },
