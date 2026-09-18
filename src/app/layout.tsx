40ed93fa4f34fcbe23be57cf9de1224c60017f0b
KlyrhonMiko <aurelklyrhonmiko@gmail.com>
2026-09-18 13:40:40 +0800

    feat: google seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   metadataBase: new URL(
-    process.env.NEXT_PUBLIC_SITE_URL ||
-      (process.env.VERCEL_PROJECT_PRODUCTION_URL
-        ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-        : process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : "http://localhost:3000")
+    process.env.NEXT_PUBLIC_SITE_URL || "https://www.klyrhon.me"
   ),
   title: "Klyrhon Aurel | Portfolio",
   description:
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,13 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
-  const siteUrl =
-    process.env.NEXT_PUBLIC_SITE_URL ||
-    (process.env.VERCEL_PROJECT_PRODUCTION_URL
-      ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-      : process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : "http://localhost:3000");
+  const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.klyrhon.me";
 
   return {
     rules: {
